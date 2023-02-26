09295c4b1f58c7538252d882f6f96a9b3a981a2f
REVANTH PHANI SAI MEDUKONDURU <96735161+revan1h@users.noreply.github.com>
2023-02-27 02:32:56 +1030

    Code Updations EOD

--- Project1/ProjectManager.sol
+++ Project1/ProjectManager.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.0 <=0.9.0;
-
 import "./oneChange.sol";
 
 contract ProjectManager{
@@ -13,7 +12,10 @@ contract Project{
         address projectOwner; // Person who has made this contract
         string projectName; // Name of the campaign.
         string projectDescription; // Description explaining - moto of this campaign.
-        uint256 projectCosts; // Amount required to finish this project.
+        uint256 estimatedProjectPrice; // Amount required to finish this project as per govt budget - So this stays as base price what govt wants to offer.
+        string companyName; // Company which is doing this project. 
+        address companyPayId; // Company's Public address to which payment is sent.
+        uint256 finalProjectPrice; // Finalised price for this tender.
         uint8 projectStatus; // 0 - Project initiated; 1 - got approval from majority of voters; 2 - organisations finalised the tender and contract closed.
     }
 
@@ -39,7 +41,10 @@ contract Project{
             projectOwner : _projectOwner,
             projectName : _projectName,
             projectDescription : _projectDescription,
-            projectCosts : _projectCosts,
+            estimatedProjectPrice : _projectCosts,
+            companyName: "",
+            companyPayId: address(0),
+            finalProjectPrice: 0,
             projectStatus : 0
         });
         projectAreas = _projectAreas;
@@ -73,6 +78,35 @@ contract Project{
         }
     }
 
+    // function for Organisations to submit their tender price
+    function submitTenderPrice(uint256 _proposedPrice) public {
+        // Tender should be only visible if it got approved by majority of the people
+        require (proposalDetails.projectStatus == 1, "This tender is not yet opened for bidding.");
 
+        // Organisation that wants to submits tender price
+        oneChange oneChangeTemp = oneChange (oneChangeContractAddress);
+        uint8 temp = oneChangeTemp.getUserLevel(msg.sender);
+        require(((temp == 3) || (temp == 4)), "To quote or enter tender price, you must first register as an organization");
+
+        // Checking whether they proposed a price less than or equal to estimated costs for this project
+        require( _proposedPrice <= proposalDetails.estimatedProjectPrice, "The quoted price is more than the estimated price for this project. Please revise your quote.");
+
+        // Updating tender price - First Come First Win Tender (If its best quote)
+        if (_proposedPrice < proposalDetails.finalProjectPrice || proposalDetails.finalProjectPrice == 0){
+            proposalDetails.finalProjectPrice = _proposedPrice;
+            proposalDetails.companyName = oneChangeTemp.getUserFullName(msg.sender);
+            proposalDetails.companyPayId = msg.sender;
+        }
+    }
+
+    // function to revise the estimated costs - In Case none of the organisation is happy to work for previous estimated price
+    function reviseEstimatedCosts(uint256 _revisedPrice) public {
+        // Caller of this function should be same as the person who deployed this contract
+        require (msg.sender == proposalDetails.projectOwner, "Only the caller who deployed this contract can call this function.");
+
+        // Update the estimated costs for this project.
+        proposalDetails.estimatedProjectPrice = _revisedPrice;
+    }
+
+    // Finalise the project 
 }
-
